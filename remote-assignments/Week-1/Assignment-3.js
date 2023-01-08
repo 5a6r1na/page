2832e8eb56bc01a5d7182f93be00fcd800278566
5a6r1na <119278456+5a6r1na@users.noreply.github.com>
2023-01-08 20:22:00 +0800

    Update Assignment-3.js

--- remote-assignments/Week-1/Assignment-3.js
+++ remote-assignments/Week-1/Assignment-3.js
@@ -1,4 +1,4 @@
-
+//Input1
 function countAandB (input1) {
 let counterA = 0;
 let counterB = 0;
@@ -12,7 +12,6 @@ let counterB = 0;
     }    
     return input1 = `${counterA + counterB} ("${counterA} 'a' letters and ${counterB} 'b' letters)`   
 }
-
 
 function toNumber (input1) {
     let newinput1 = [];
@@ -31,12 +30,11 @@ function toNumber (input1) {
     return input1 = newinput1;
 }
 
-
-
 let input1 =  ['a', 'b', 'c', 'a', 'c', 'a', 'c'];
 console.log(countAandB(input1));
 console.log(toNumber(input1));
 
+//Input2
 function countAandB (input2) {
     let counterA = 0;
     let counterB = 0;
@@ -50,26 +48,23 @@ function countAandB (input2) {
         }    
         return input2 = `${counterA + counterB} ("${counterA} 'a' letters and ${counterB} 'b' letters)`   
     }
-    
-    
-    function toNumber (input2) {
-        let newinput2 = [];
-        input2.forEach(function(item){
-            if (item === 'a') {
-                newinput2.push('1');
-            } else if (item === 'b') {
-                newinput2.push('2');
-            } else if (item ==='c') {
-                newinput2.push('3');
-            } else if (item ==='d') {
-                newinput2.push('4');
-            } else if (item ==='e') {
-                newinput2.push('5');}
-        });
+        
+function toNumber (input2) {
+    let newinput2 = [];
+    input2.forEach(function(item){
+        if (item === 'a') {
+            newinput2.push('1');
+        } else if (item === 'b') {
+            newinput2.push('2');
+        } else if (item ==='c') {
+            newinput2.push('3');
+        } else if (item ==='d') {
+            newinput2.push('4');
+        } else if (item ==='e') {
+            newinput2.push('5');}
+      });
         return input2 = newinput2;
     }
-    
-    
     
     let input2 = ['e', 'd', 'c', 'd', 'e'];
     console.log(countAandB(input2));
